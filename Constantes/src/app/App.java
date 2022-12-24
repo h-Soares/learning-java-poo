d21859c4a47e38ffec60e85fc7fcb78eadf2de04
h-Soares <hiagosoares002@gmail.com>
2022-12-24 11:48:13 -0300

    Adicionado teoria sobre o uso de final e static em constantes.

---
+++ Constantes/src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+import entities.Constants;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(Constants.URL_SITE);
+    }
+}
---
+++ Constantes/src/entities/Constants.java
@@ -0,0 +1,7 @@
+package entities;
+
+public final class Constants { //classe que não pode ser herdada, FINAL.
+    public final static String URL_SITE = "https://mysite.com"; 
+    public final static int SITE_CEO = 2;
+}//final para não ser modificado e static para usar sem instanciar um objeto.
+//o padrão de escrita de constantes é esse: caixa alta e underscore.
