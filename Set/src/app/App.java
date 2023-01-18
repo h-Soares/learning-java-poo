dce22f490ae14c4fb1ccd0a7ba3de9baf69de84b
h-Soares <hiagosoares002@gmail.com>
2023-01-18 14:21:58 -0300

    Atualizando teoria sobre Set.

--- Set/src/app/App.java
+++ Set/src/app/App.java
@@ -7,11 +7,13 @@ public class App {
         elementos não possuem posição;
         acesso, inserção e remoção de elementos são rápidos;
         tem operações eficientes de conjuntos: intersecção, união, diferença;
+        Quando usar: quando a ordem não importar e não se quer repetições. É útil, também, para excluir as repetições de um ArrayList, com addAll ou pelo Construtor.
         Implementações:
-            HashSet - mais rápido ( O(1) em tabela hash ) e não ordenado.
+            HashSet - mais rápido ( O(1) em tabela hash ) e não ordenado. É A MAIS USADA, pois é a MAIS RÁPIDA.
             TreeSet - mais lento ( O(longN) em árvore rubro-negra) e ordenado pelo compareTo ou Comparator do objeto. T tem que implementar Comparable<T> e 
-                implementar compareTo.
-            LinkedHashSet - velocidade intermediária e elementos na ordem em que são adicionados. 
+                implementar compareTo. Por padrão, é ordenado em ordem ascendente.
+            LinkedHashSet - velocidade intermediária e elementos na ordem em que são adicionados, pois é implementado como uma lista duplamente encadeada, em que
+                cada elemento tem um ponteiro apontando para o elemento anterior e para o próximo. Segundo mais rápido.
         Métodos importantes:
             add(obj), remove(obj), contains(obj), são baseados em EQUALS e HASH CODE. Se equals e hashCode não existirem, é usada a comparação de ponteiros.
             clear(), size(), removeIf(predicate)
