0711d2fd58d139d417c8a878ef958c7e034b6d90
h-Soares <hiagosoares002@gmail.com>
2023-01-19 18:53:59 -0300

    Teoria sobre Map atualizada.

--- Map/src/app/App.java
+++ Map/src/app/App.java
@@ -1,5 +1,8 @@
 package app;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) {
         /* Map<K,V> : K = Key;  V = Value.
@@ -16,8 +19,34 @@ public class App {
 
         Métodos importantes:
         put(key,value) , remove(key) - remove a chave e o value associado a ela , containsKey(key) , get(key) - retorna o value associado a key. Se essa key não existir, retorna null. | são baseados em hashCode e equals.
-            Se hashCode e equals não existirem, é usada a comparação de ponteiros.
+            Se hashCode e equals não existirem NA KEY (tem que ter) (também implementar em VALUE), é usada a comparação de ponteiros.
         clear(), size(), keySet() - retorna um Set<K> , values() - retorna um Collection<V>
          */
+        
+        Map<String, String> infos = new HashMap<>();
+
+        infos.put("username", "jhon");
+        infos.put("email", "jhon321@gmail.com");
+        infos.put("phone", "401575425");
+
+
+        //infos.remove("phone");
+        //infos.put("phone", "32155");
+
+        for(String info : infos.keySet())
+            System.out.println(info + ": " + infos.get(info));
+
+        System.out.println(infos.containsKey("username"));
+
+        Map<String, Integer> election = new HashMap<>();
+        election.put("Alex", 10);
+        election.put("Joana", 15);
+        System.out.println(election);
+        System.out.println();
+
+        if(election.containsKey("Alex")) { //fazer assim no exercício
+            election.replace("Alex", election.get("Alex") + 10);
+        }
+        System.out.println(election);
     }
 }
