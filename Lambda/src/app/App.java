3867d1c8c5a9bb8488ff2ce4f967adfd9da10e40
h-Soares <hiagosoares002@gmail.com>
2023-01-21 01:18:54 -0300

    Teoria sobre Lambda adicionada.

---
+++ Lambda/src/app/App.java
@@ -0,0 +1,24 @@
+package app;
+public class App {
+    public static void main(String[] args) {
+        /* Expressão lambda é uma FUNÇÃO anônima de primeira classe.
+        Função anônima: função não declarada, sem nome.
+        De primeira classe: pode ser passada como argumento para outras funções. Também pode ser retornada.
+
+        Uma interface funcional é uma interface que contém APENAS UM MÉTODO ABSTRATO (obrigatório implementar). Pode ter defaults.
+            Suas implementações podem ser tratadas como expressões lambda, ao invés de criar uma classe e implementar.
+            Isso quer dizer que uma interface funcional tem um mapeamento direto com uma expressão lambda.
+            "Podemos dizer que uma expressão lambda representa a implementação do método abstrato da interface funcional".
+            O Lambda PRECISA estar em uma associação com uma interface funcional! 
+        
+        Programação imperativa se preocupa com como o problema será resolvido nos mínimos detalhes, os passos etc. Java, no geral,
+            é assim.
+        Já na programação funcional, que é declarativa, nos preocupamos mais com o que precisa ser resolvido, feito. 
+            O processo imperativo ainda existe, mas a sequência de comandos é abstraída pela API do Java, e nós temos que 
+            apenas dizer o que fazer.
+        
+        A grande vantagem de funções lambda é diminuir a quantidade de código necessária para a escrita de algumas funções.
+            
+         */
+    }
+}
